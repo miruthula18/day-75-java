dd0ed6af5d7d696fb1fedf2e4b48bf6af25be380
miruthula <miruthula183@gmail.com>
2026-09-17 10:06:19 -0700

    Day 75 - Find maximum using ArrayList

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(sc.nextInt());
+        }
+        int max = list.get(0);
+        for (int num : list) {
+            if (num > max) {
+                max = num;
+            }
+        }
+        System.out.println(max);
+    }
+}
